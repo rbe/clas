9849cf7bf629af181f9b875a3a2aec305b22d158
Ralf Bensmann <ralf@art-of-coding.eu>
2012-05-22 15:19:18 +0200

    Some cleanup

--- project.clj
+++ project.clj
@@ -1,19 +1,17 @@
 (defproject clas "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.stuartsierra/lazytest "1.1.2"]
-                 [ring/ring-core "0.3.8"]
-                 [ring/ring-jetty-adapter "0.3.8"]
+                 [ring/ring-core "1.1.0"]
+                 [ring/ring-jetty-adapter "1.1.0"]
                  [org.danlarkin/clojure-json "1.1"]
                  [http.async.client "0.2.0"]
                  [clj-time "0.1.0-SNAPSHOT"]
                  [com.draines/postal "1.4.0-SNAPSHOT"]
                  [org.clojars.kjw/mysql-connector "5.1.11"]]
-  :dev-dependencies
-                 [[ring/ring-devel "0.3.8"]
-		  [swank-clojure "1.2.1"]]
-  :jvm-opts      ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8030"]
+  :dev-dependencies [[ring/ring-devel "1.1.0"]
+                     [swank-clojure "1.2.1"]]
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8030"]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
   :source-path "src/:src/appmodule/:src/proxymodule/"
   :omit-source true
--- src/com/bensmann/clas/config.clj
+++ src/com/bensmann/clas/config.clj
@@ -11,26 +11,24 @@
   (:use (com.bensmann.clas config log))
   (:require [com.bensmann.clas [fpersist :as f]]))
 
-(def *config-vars* (agent {
-			   :development-mode {:enabled false :value nil}
-                           }))
+(def *config-vars* (agent {:development-mode {:enabled false :value nil}}))
 
 (defn
   ^{
-    :doc "Get value for config-var."
-    :added ""
-    :user/comment "A comment."
-    }
+     :doc "Get value for config-var."
+     :added ""
+     :user/comment "A comment."
+     }
   get-cv
   [k]
-  (get-in @*config-vars* [k :value]))
+  (get-in @*config-vars* [k :value ]))
 
 (defn
   ^{
-    :doc "Set value for config-var."
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Set value for config-var."
+     :added ""
+     :user/comment "A comment."
+     }
   set-cv
   [k v]
   (let [cv (get-in @*config-vars* [k])]
@@ -39,55 +37,55 @@
       (dosync
         (send *config-vars* assoc k {:value v :enabled false}))
       (dosync
-        (send *config-vars* assoc-in [k :value] v)))))
+        (send *config-vars* assoc-in [k :value ] v)))))
 
 (defn
   ^{
-    :doc "Enable a config-var."
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Enable a config-var."
+     :added ""
+     :user/comment "A comment."
+     }
   enable-cv
   [k]
   (dosync
-    (send *config-vars* assoc-in [k :enabled] true)))
+    (send *config-vars* assoc-in [k :enabled ] true)))
 
 (defn
   ^{
-    :doc "Disable a config-var."
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Disable a config-var."
+     :added ""
+     :user/comment "A comment."
+     }
   disable-cv
   [k]
   (dosync
-    (send *config-vars* assoc-in [k :enabled] false)))
+    (send *config-vars* assoc-in [k :enabled ] false)))
 
 (defn
   ^{
-    :doc "Is config-var enabled?"
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Is config-var enabled?"
+     :added ""
+     :user/comment "A comment."
+     }
   cv-enabled?
   [k]
-  (get-in @*config-vars* [k :enabled]))
+  (get-in @*config-vars* [k :enabled ]))
 
 (defn
   ^{
-    :doc "Is config-var disabled?"
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Is config-var disabled?"
+     :added ""
+     :user/comment "A comment."
+     }
   cv-disabled?
   [k]
-  (not= true (get-in @*config-vars* [k :enabled])))
+  (not= true (get-in @*config-vars* [k :enabled ])))
 
 (defmacro
   ^{
-    :doc "Execute code when a config-var is enabled."
-    :user/comment "A comment."
-   }
+     :doc "Execute code when a config-var is enabled."
+     :user/comment "A comment."
+     }
   with-cv-enabled
   [k & forms]
   (if (cv-enabled? k)
@@ -95,9 +93,9 @@
 
 (defmacro
   ^{
-    :doc "Execute code when a config-var is disabled."
-    :user/comment "A comment."
-   }
+     :doc "Execute code when a config-var is disabled."
+     :user/comment "A comment."
+     }
   with-cv-disabled
   [k & forms]
   (if (cv-disabled? k)
@@ -105,39 +103,39 @@
 
 (defn
   ^{
-    :doc "Show a config-var in REPL."
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Show a config-var in REPL."
+     :added ""
+     :user/comment "A comment."
+     }
   show-cv
   [k]
   (let [cv (k @*config-vars*)
         e (if (:enabled cv) "+" "-")
-	v (or (:value cv) "no value")]
+        v (or (:value cv) "no value")]
     (format "%s %s = %s" e (name k) v)))
 
 (defn
   ^{
-    :doc "Persist actual config-vars in a file."
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Persist actual config-vars in a file."
+     :added ""
+     :user/comment "A comment."
+     }
   save-cv
   [& file]
   (f/save-map (or file "config-vars.clj") @*config-vars*))
 
 (defn
   ^{
-    :doc "Load config-vars from file."
-    :added ""
-    :user/comment "A comment."
-   }
+     :doc "Load config-vars from file."
+     :added ""
+     :user/comment "A comment."
+     }
   load-cv
   [& file]
   (if-let [cv-form (f/load-map (or file "config-vars.clj"))]
     (do
       (dosync
-       (send *config-vars* merge cv-form))
+        (send *config-vars* merge cv-form))
       (await *config-vars*)
       true)
     false))
